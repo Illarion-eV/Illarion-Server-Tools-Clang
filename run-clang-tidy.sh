fed8d8138cb28986af20aa5208886aebb8a88bbd
vilarion <vilarion@illarion.org>
2020-11-18 13:49:54 +0100

    Use .clang-tidy config

--- run-clang-tidy.sh
+++ run-clang-tidy.sh
@@ -1,8 +1,8 @@
 cd /build
 cmake /src
-python3 /usr/local/share/clang/run-clang-tidy.py -j $(nproc) -checks="$1" -header-filter=/src/src/.* /src/src/.* > tidy.out
+python3 /usr/local/share/clang/run-clang-tidy.py -j $(nproc) /src/src/.* > tidy.out
 cat tidy.out | sed '/^clang-tidy/,$!d' | sed '/^clang-tidy/d' | awk '!a[$0]++' RS="/src/src/" ORS="" > tidy.deduplicated
-sed -i '/\/src\/src\///g' tidy.deduplicated
+sed -i '/\/src\/src\//src\//g' tidy.deduplicated
 cat tidy.deduplicated
 echo "\nSummary:\n"
 grep -oP '\[.*?-.*?\]' tidy.deduplicated | sort | uniq -c | sort -nr | tee /dev/tty | awk '{total = total + $1}END{print "  ",total,"warnings"}'
